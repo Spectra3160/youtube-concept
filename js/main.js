d4b011995cf5493c93fe91c501ffad92ded15471
Spectra3160 <81949859+Spectra3160@users.noreply.github.com>
2021-04-09 23:30:01 +1000

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+     slidesPerView: 6 
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1200: {
+     slidesPerView: 4 
+    },
+    1992: {
+     slidesPerView: 3 
+    },
+    760: {
+     slidesPerView: 2 
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
@@ -9,3 +27,12 @@ const swiper = new Swiper('.channel-slider', {
   },
 
 });
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  myChannelSlider.destroy();
+}
