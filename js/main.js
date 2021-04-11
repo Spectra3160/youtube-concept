49058bd943b229b402b63fe52b2f78bf7afe181a
Spectra3160 <81949859+Spectra3160@users.noreply.github.com>
2021-04-11 21:21:47 +1000

    Добавила рекомендации и еду с напитками

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const swiper = new Swiper('.channel-slider', {
     1200: {
      slidesPerView: 4 
     },
-    1992: {
+    992: {
      slidesPerView: 3 
     },
     760: {
@@ -27,6 +27,61 @@ const swiper = new Swiper('.channel-slider', {
   },
 
 });
+
+const swiper2 = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+     slidesPerView: 3 
+    },
+    1200: {
+     slidesPerView: 2 
+    },
+    760: {
+     slidesPerView: 1 
+    }
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+
+});
+
+const swiper3 = new Swiper('.recommended-channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+     slidesPerView: 6 
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1200: {
+     slidesPerView: 4 
+    },
+    992: {
+     slidesPerView: 3 
+    },
+    760: {
+     slidesPerView: 2 
+    }
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-channel-button-next',
+    prevEl: '.recommended-channel-button-prev',
+  },
+
+});
+
 const searchBtn = document.querySelector('.mobile-search');
 const mobileSearch = document.querySelector('.input-group');
 searchBtn.addEventListener('click', () => {
